773876c462b52a571be78792f376636f17ce6b50
Amarok-w <Vadimon4k00073@gmail.com>
2021-09-08 19:12:41 +0300

    added slider into models section

---
+++ script.js
@@ -0,0 +1,32 @@
+const leftArrow = document.querySelector('.l-arrow');
+const rightArrow = document.querySelector('.r-arrow');
+const modelsGrid = document.querySelector('.models-grid');
+
+let counter = 0;
+let indent = 0;
+// 375
+// 750
+// 1125
+
+leftArrow.addEventListener('click', () => {
+  if (counter == 0) {
+    counter = 3;
+    indent = 1125;
+    modelsGrid.style.marginLeft = `-${indent}px`;  
+  } else {
+    counter -= 1;
+    indent -= 375;
+    modelsGrid.style.marginLeft = `-${indent}px`;  
+  }
+})
+rightArrow.addEventListener('click', () => {
+  if (counter == 3) {
+    counter = 0;
+    indent = 0;
+    modelsGrid.style.marginLeft = '0px';  
+  } else {
+    counter += 1;
+    indent += 375;
+    modelsGrid.style.marginLeft = `-${indent}px`;  
+  }
+})
