2e9a2ea11b87e33552583b1a48e73a4abb4cab30
Amarok-w <Vadimon4k00073@gmail.com>
2021-09-08 20:42:13 +0300

    slider in header section

--- script.js
+++ script.js
@@ -1,29 +1,67 @@
-const leftArrow = document.querySelector('.l-arrow');
-const rightArrow = document.querySelector('.r-arrow');
-const modelsGrid = document.querySelector('.models-grid');
+function modelsSlider() {
+  
+  const leftArrow = document.querySelector('.l-arrow');
+  const rightArrow = document.querySelector('.r-arrow');
+  const modelsGrid = document.querySelector('.models-grid');
+  
+  let counter = 0;
+  let indent = 0;
+  
+  leftArrow.addEventListener('click', () => {
+    if (counter == 0) {
+      counter = 3;
+      indent = 1125;
+      modelsGrid.style.marginLeft = `-${indent}px`;  
+    } else {
+      counter -= 1;
+      indent -= 375;
+      modelsGrid.style.marginLeft = `-${indent}px`;  
+    }
+  })
+  rightArrow.addEventListener('click', () => {
+    if (counter == 3) {
+      counter = 0;
+      indent = 0;
+      modelsGrid.style.marginLeft = '0px';  
+    } else {
+      counter += 1;
+      indent += 375;
+      modelsGrid.style.marginLeft = `-${indent}px`;  
+    }
+  })
 
-let counter = 0;
-let indent = 0;
+}
 
-leftArrow.addEventListener('click', () => {
-  if (counter == 0) {
-    counter = 3;
-    indent = 1125;
-    modelsGrid.style.marginLeft = `-${indent}px`;  
-  } else {
-    counter -= 1;
-    indent -= 375;
-    modelsGrid.style.marginLeft = `-${indent}px`;  
-  }
-})
-rightArrow.addEventListener('click', () => {
-  if (counter == 3) {
-    counter = 0;
-    indent = 0;
-    modelsGrid.style.marginLeft = '0px';  
-  } else {
-    counter += 1;
-    indent += 375;
-    modelsGrid.style.marginLeft = `-${indent}px`;  
-  }
-})
+modelsSlider();
+
+
+function headerSlider() {
+
+  const slideMarks = document.querySelectorAll('.slide-mark');
+  const sliderLine = document.querySelector('.slider-line');
+  let current = 0;
+
+
+
+  slideMarks.forEach(slideMark => {
+    slideMark.addEventListener('click', el => {
+
+      slideMarks.forEach(elem => {
+        elem.classList.remove('slider-mark_current');
+      })
+
+      el.target.classList.add('slider-mark_current');
+
+      current = 100 * el.target.classList[1].substring(el.target.classList[1].length -1);
+
+      sliderLine.style.right = `${current}%`;
+      
+    })
+  })
+
+}
+
+headerSlider();
+
+
+
